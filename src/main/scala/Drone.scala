0ca43a5e36343ce99229f15d5cfb26fa244872ed
Hadarak <nico_roche@hotmail.fr>
2020-06-08 05:26:08 -0700

    add src

---
+++ src/main/scala/Drone.scala
@@ -0,0 +1,16 @@
+class Drone() {
+  var latitude:Float = 0
+  var longitude:Float = 0
+  var id:Integer = 0
+  var timestamp:Integer = 0
+
+  def update: Unit = {
+      //update les datas
+  }
+
+  def send: Unit = {
+
+  }
+
+}
+
